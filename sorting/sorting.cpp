414d9b0c1cb15232765a96bfd41bf1195a7c17f2
Colin Zhe <colin.zhe@amd.com>
2021-02-26 22:40:34 -0500

    updated .gitignore and submitted complete merge sort

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -3,6 +3,15 @@
 
 #include <iostream>
 
+void printArr(int* arr, int len)
+{
+    for (int i{ 0 }; i < len; ++i)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
 // insertion sort
 // avg = worst case = O(n^2)
 bool insertSort(int* arr, int len)
@@ -30,32 +39,75 @@ bool insertSort(int* arr, int len)
     return true;
 }
 
+// merge array for merge sort
 void mergeArr(int* arr, int p, int q, int r)
 {
-    int lSize = q - p + 1;
-    int rSize = r - q + r;
+    int lSize = q - p + 1; // size of left sub-array
+    int rSize = r - q; // size of right sub-array
+
+    int* lArr{ new int[lSize] }; // temp array to hold left sub-array
+    int* rArr{ new int[rSize] }; // temp array to hold righ sub-array
+
+    for (int i{ 0 }; i < lSize; ++i) // fill left sub-array
+    {
+        lArr[i] = arr[p + i];
+    }
+
+    for (int j{ 0 }; j < rSize; ++j) // fill right sub-array
+    {
+        rArr[j] = arr[q + 1 + j];
+    }
+
+    // init indices
+    int lIndex{ 0 };
+    int rIndex{ 0 };
+    int arrIndex{ p };
+
+    while ((lIndex < lSize) && (rIndex < rSize)) // keep going until one of sub-array's end is reached
+    {
+        if (lArr[lIndex] < rArr[rIndex]) // if left element is smaller, put it in main array
+        {
+            arr[arrIndex] = lArr[lIndex];
+            ++lIndex; // shift to next position in left sub-array
+        }
+        else // if right element is smaller, put it in main array
+        {
+            arr[arrIndex] = rArr[rIndex];
+            ++rIndex; // shift to next position in right sub-array
+        }
+        ++arrIndex; // shift to next position in main array each time it is populated
+    }
+
+    while (lIndex < lSize) // fill rest of left sub-array into main array (assuming right sub-array reached end first from above)
+    {
+        arr[arrIndex] = lArr[lIndex];
+        ++lIndex;
+        ++arrIndex;
+    }
+
+    while (rIndex < rSize) // fill rest of right sub-array into main array (assuming left sub-array reached end first from above)
+    {
+        arr[arrIndex] = rArr[rIndex];
+        ++rIndex;
+        ++arrIndex;
+    }
+
+    // delete temp arrays
+    delete[] lArr;
+    delete[] rArr;
 }
 
 // merge sort
 // worse case = O(n * log n)
 void mergeSort(int* arr, int p, int r)
 {
-    if (r > p)
+    if (r > p) // keep going as long as size of arr is not 1
     {
-        int q = (p + r) / 2;
-        mergeSort(arr, p, q);
-        mergeSort(arr, q + 1, r);
-        mergeArr(arr, p, q, r);
+        int q = p + (r - p) / 2; // becareful to add p as base, since p not always == 0
+        mergeSort(arr, p, q); // recursively sort left side
+        mergeSort(arr, q + 1, r); // recursively sort right side
+        mergeArr(arr, p, q, r); // merge and actually sort two sub-arrays
     }
-}
-
-void printArr(int* arr, int len)
-{
-    for (int i{ 0 }; i < len; ++i)
-    {
-        std::cout << arr[i] << ' ';
-    }
-    std::cout << '\n';
 }
 
 int main()
